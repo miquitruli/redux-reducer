cf8f7e8fbd895062758d1201d761a25b8c54e931
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-18 19:39:08 -0500

    commit tree cbbef6bf24cdbdc94a68f1578a110f60b66185ec

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,26 @@
+let state = { friends: [] };
+
 export function manageFriends(state, action){
+    switch(action.type){
+        case 'ADD_FRIEND':
+            return (
+                {...state, friends: [...state.friends, action.friend] }
+            )
+
+        case 'REMOVE_FRIEND':
+            let removedFriend = state.friends.findIndex(friend => friend.id === action.id)
+            return (
+                {...state, friends: [
+                    ...state.friends.slice(0, removedFriend),
+                    ...state.friends.slice(removedFriend + 1)
+                ]
+            }
+            )
+        default:
+            return state;
+    }
 }
+
+
+
+
